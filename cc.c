213b2a5283f2bc6ee28eb404a0ca0e8487a614fa
myoungin <myoungin729@gmail.com>
2022-05-06 16:24:42 +0900

    c programming

--- cc.c
+++ cc.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 #define _CRT_SECURE_NO_WARNINGS
-int main(void){
-    int s=0;
-    int i=1;
-    while (i<=10){
-        if ((i%3==0) || (i%5==0)){
-            s += i;
 
-        }
-        i++;
+int main(void){
+    char A[20];
+    char B[20];
+    char *pa, *pb;
+    int len1 = 0, len2 = 0;
+    int max_len = 0;
+
+    scanf("%s",A);
+    scanf("%s",B);
+    len1 = strlen(A);
+    len2 = strlen(B);
+
+    if (len1 < len2){
+        max_len = len2;
     }
-    printf("3 또는 5의 배수의 합은 %d",s);
+    else {
+        max_len = len1;
+    }
+
+    pa = A;
+    pb = B;
+
+    int i;
+    for (i=0;i<max_len;i++){
+        if (pa[i] == pb[i]){
+            printf("O");
+        }
+        else {
+            printf("X");
+        }
+    }
+    printf("\n");
     return 0;
 } 
