291bc0dea99a443bbb32cd3a9ede40a448cc52a8
myoungin <myoungin729@gmail.com>
2022-05-09 16:10:06 +0900

    c_exercise

--- c_stru.c
+++ c_stru.c
@@ -12,38 +12,44 @@ int main(void){
     int i,j,k,t;
     int temp_len=0, word_count=0, space=0;
     int start=0, end=0, p_idx=0;
-    for (i=0;i<2;i++){
+    for (i=0;i<2;i++)
+    {
         gets(temp);
         temp_len = strlen(temp);
-        for (j=0;j<temp_len;j++){
-            if (temp[j]==' ') {
+        j = 0;
+        while (temp[j])
+        {
+            if (temp[j++]==' ')
                 space += 1;
-            }
-            word_count = space + 1;
-            WORD* p = (WORD*)malloc(word_count*sizeof(WORD));  
-            start = 0;
-            ptemp = temp;
-            p_idx = 0;
-            for (j=0;j<temp_len;j++){
-                if (temp[j]==' ' || temp[j]=='\0'){
-                    end = j;
-                    p[p_idx].str = (char*)malloc((end-start+1)*sizeof(char));
-                    strncpy(p[p_idx].str, ptemp, end-start+1);
-                    p[p_idx].str[end-start] = '\0';
-                    ptemp = ptemp + end - start + 1;
-                    p[p_idx].length = end-start;
-                    p_idx++;
-                    start = end + 1;
-                }
-            }
-            for(k=0;k<p_idx;k++){
-                printf("%s, %d\n", p[k].str, p[k].length);
-            }
-            for (t=0;t<p_idx;t++){
-                free(p[t].str);
-            }
-            free(p);
         }
+        word_count = space + 1;
+        WORD* p = (WORD*)malloc(word_count*sizeof(WORD));  
+        start = 0;
+        ptemp = temp;
+        p_idx = 0;
+        j = 0;
+        while (1)
+        {
+            if (temp[j]==' ' || temp[j]=='\0')
+            {
+                end = j;
+                p[p_idx].str = (char*)malloc((end-start+1)*sizeof(char));
+                strncpy(p[p_idx].str, ptemp, end-start+1);
+                p[p_idx].str[end-start] = '\0';
+                ptemp = ptemp + end - start + 1;
+                p[p_idx].length = strlen(p[p_idx].str);
+                p_idx++;
+                start = end + 1;
+            }
+            if (!temp[j])
+                break ;
+            j++;
+        }
+        for(k=0;k<p_idx;k++)
+            printf("%s, %d\n", p[k].str, p[k].length);
+        for (t=0;t<p_idx;t++)
+            free(p[t].str);
+        free(p);
     }
     return 0;
 }
